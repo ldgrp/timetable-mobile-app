c3994149077d205aaef36745f3500d2e0737befc
unknown <leoorpilla3@gmail.com>
2017-08-19 17:18:12 +1000

    cleanup

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,6 +1,4 @@
-import userActions from './user';
 import subjectActions from './subjects'
 module.exports = {
-    ...userActions,
     ...subjectActions,
 };
--- js/reducers/index.js
+++ js/reducers/index.js
@@ -2,7 +2,5 @@ import { combineReducers } from 'redux';
 
 module.exports = combineReducers({
     nav: require('./nav.js'),
-    title: require('./title.js'),
-    user: require('./user.js'),
     subjects: require('./subjects.js'),
 })
